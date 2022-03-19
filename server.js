e0223e5030f0be4699dd07f74ba97e4bd0c07118
quanghuytran172 <quanghuytran172@gmail.com>
2022-03-19 17:06:06 +0700

    fix port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 8000;
 
 server.use("/api/v1", middlewares);
 server.use("/api/v1", router);
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("JSON Server is running");
 });
