eaa811e3492ff054d129b792e317841fc22e69d3
quanghuytran172 <quanghuytran172@gmail.com>
2022-03-19 16:17:32 +0700

    fix config

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const middlewares = jsonServer.defaults({
 });
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
-
-server.use("/api", router);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+);
+server.use(router);
 
 server.listen(PORT, () => {
     console.log("Server is running");
