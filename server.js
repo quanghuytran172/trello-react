030d69cca471a0c48895ddc3f00c4183e99e7ec0
quanghuytran172 <quanghuytran172@gmail.com>
2022-03-19 16:52:35 +0700

    Fix route config

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-    static: "./build",
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    })
-);
-server.use(router);
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-server.listen(PORT, () => {
-    console.log("Server is running");
+server.use("/api/v1", middlewares);
+server.use("/api/v1", router);
+server.listen(3000, () => {
+    console.log("JSON Server is running");
 });
